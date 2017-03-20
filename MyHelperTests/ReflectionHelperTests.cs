d79641537eb11bee380e1370b6e6c6048d6427b0
shenjl <selonsy@gmail.com>
2017-03-20 18:47:24 +0800

    add unit test

---
+++ MyHelperTests/ReflectionHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Devin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Devin.Tests
+{
+    [TestFixture()]
+    public class ReflectionHelperTests
+    {
+        [Test()]
+        public void CreateClassInstanceTest()
+        {
+            object obj = ReflectionHelper.CreateClassInstance("Devin.Services.apiService,Devin_aliyun");
+            MethodInfo methodInfo = ReflectionHelper.CreateMethodInstance(obj, "gettoken");
+            string value = ReflectionHelper.DoInvoke<string>(obj, methodInfo, new object[] { "nihao" });
+            Assert.IsNotNull(value);
+        }
+    }
+}
